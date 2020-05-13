842c80a431def7991b691ff983db0a2cd98dfa46
OURAY <omerfarukuray5@gmail.com>
2020-05-13 14:07:10 +0300

    Initial Commit

---
+++ catchTheCannyGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  catchTheCannyGame
+//
+//  Created by Ömer Uray on 13.05.2020.
+//  Copyright © 2020 Ömer Uray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
